7627f5bdb0424f5adb052d82eda1e330460c74ee
Jappreet Singh <jmultani12@gmail>
2020-10-07 10:34:16 +0530

    Updates

---
+++ precache-manifest.3e4c1f5d23682e2ea42e6282f11f373c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68dc1e54d420f691d5ccae8c8e0c9e53",
+    "url": "/Airbnb-clone/index.html"
+  },
+  {
+    "revision": "f84546aa2f7a85f39a48",
+    "url": "/Airbnb-clone/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "2c13ab906600b31ce8bf",
+    "url": "/Airbnb-clone/static/css/main.9f2684b2.chunk.css"
+  },
+  {
+    "revision": "f84546aa2f7a85f39a48",
+    "url": "/Airbnb-clone/static/js/2.b044fd61.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/Airbnb-clone/static/js/2.b044fd61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c13ab906600b31ce8bf",
+    "url": "/Airbnb-clone/static/js/main.030d2760.chunk.js"
+  },
+  {
+    "revision": "2444f55e81115dd66e26",
+    "url": "/Airbnb-clone/static/js/runtime-main.fc129058.js"
+  }
+]);
